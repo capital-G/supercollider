e521593fc00d2e3aaf66bbb35738d0ff7cf4e606
Dennis Scheiba <dennis.scheiba@gmx.de>
2024-03-08 22:51:05 +0100

    apply linting

--- common/boost_string_file.hpp
+++ common/boost_string_file.hpp
@@ -24,31 +24,28 @@
 
 #include <boost/filesystem/detail/header.hpp> // must be the last #include
 
-namespace boost {
-namespace filesystem {
+namespace boost { namespace filesystem {
 
-inline void save_string_file(path const& p, std::string const& str)
-{
+inline void save_string_file(path const& p, std::string const& str) {
     filesystem::ofstream file;
     file.exceptions(std::ios_base::failbit | std::ios_base::badbit);
     file.open(p, std::ios_base::binary);
     const std::size_t sz = str.size();
-    if (BOOST_UNLIKELY(sz > static_cast< boost::uintmax_t >((std::numeric_limits< std::streamsize >::max)())))
+    if (BOOST_UNLIKELY(sz > static_cast<boost::uintmax_t>((std::numeric_limits<std::streamsize>::max)())))
         BOOST_FILESYSTEM_THROW(std::length_error("String size exceeds max write size"));
-    file.write(str.c_str(), static_cast< std::streamsize >(sz));
+    file.write(str.c_str(), static_cast<std::streamsize>(sz));
 }
 
-inline void load_string_file(path const& p, std::string& str)
-{
+inline void load_string_file(path const& p, std::string& str) {
     filesystem::ifstream file;
     file.exceptions(std::ios_base::failbit | std::ios_base::badbit);
     file.open(p, std::ios_base::binary);
     const boost::uintmax_t sz = filesystem::file_size(p);
-    if (BOOST_UNLIKELY(sz > static_cast< boost::uintmax_t >((std::numeric_limits< std::streamsize >::max)())))
+    if (BOOST_UNLIKELY(sz > static_cast< boost::uintmax_t>((std::numeric_limits<std::streamsize>::max)())))
         BOOST_FILESYSTEM_THROW(std::length_error("File size exceeds max read size"));
-    str.resize(static_cast< std::size_t >(sz), '\0');
+    str.resize(static_cast<std::size_t>(sz), '\0');
     if (sz > 0u)
-        file.read(&str[0], static_cast< std::streamsize >(sz));
+        file.read(&str[0], static_cast<std::streamsize>(sz));
 }
 
 } // namespace filesystem
